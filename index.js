bf02a14d5479fc1984fc8d51559d69fce193c4a7
Hritik Panchasara <hritik09@cometnet-10-21-60-110.utdallas.edu>
2019-09-01 18:30:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');     // import express  
+const app = express();                  // creating Express app
+
+app.get('/', (req, res) => {            // forward slash refers to the route, with req as incoming object
+    res.send({ hi: 'there' });          // and res as outgoing response
+});
+
+const PORT = process.env.PORT || 5000;  // look in the underlying environment to see for established ports, or use 5000
+app.listen(PORT);                       // instructs express to tell node that it wants to listen to incoming traffic from PORT
